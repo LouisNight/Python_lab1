fb6891277d344eae3b45e502da80995c95c90764
Im-Night <louisnight@pm.me>
2024-08-29 11:10:12 -0700

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,33 @@
+import math
+
+area_of_circle = math.pi*(5*5)
+print(area_of_circle)
+
+volume_of_sphere = 4/3*math.pi*3**3
+print(volume_of_sphere)
+
+triangle_sides = 3**2 + 4**2
+triangle_hypotenuse = math.sqrt(triangle_sides)
+print(triangle_hypotenuse)
+
+full_name = "Louis Night"
+print(len(full_name))
+
+fn = "Louis"
+ln = "Night"
+print(fn + ' ' + ln)
+
+print(full_name.upper())
+print(full_name.lower())
+
+age = 25
+print(type(age))
+
+height = 5.75
+print(type(height))
+
+weight = 190
+print(type(weight))
+
+bmi = 190/68**2 * 703
+print(bmi)
